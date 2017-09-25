73cbc94cbe576e8e46bbd0f256d87196c341b5bd
イシイケンタ <iyemon018@gmail.com>
2017-09-25 23:50:07 +0900

    ・見た目をMaterial Designに合わせた。 ・番号リストのフォントサイズを調整できるようにした。

--- iBingo/src/iBingo/ViewModels/ShellViewModel.cs
+++ iBingo/src/iBingo/ViewModels/ShellViewModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace iBingo.ViewModels
 {
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Windows.Input;
     using iBingo.Commands;
@@ -10,41 +11,53 @@
 
     public class ShellViewModel : WindowViewModelBase
     {
+        private static readonly Dictionary<int, double> NumbersFontSizeMap
+            = new Dictionary<int, double>
+              {
+                  {0, 24.0},
+                  {1, 36.0},
+                  {2, 48.0},
+                  {3, 60.0},
+                  {4, 72.0},
+                  {5, 96.0},
+                  {6, 128.0},
+              };
+
         #region Fields
 
         private readonly ShuffleValues _shuffleValues;
 
         private int _currentNumber;
 
+        private double _numbersFontSize;
+
         private bool _shuffling;
+
+        private bool _visibleHitNumbersView;
+
+        private bool _visibleShuffleView = true;
+
+        private int _numbersFontSizeIndex = 0;
 
         #endregion
 
         #region Ctor
 
         public ShellViewModel(IServicesProvider servicesProvider, IDialogService dialogService, IDataStore dataStore)
-            : base(servicesProvider,
-                dialogService,
-                dataStore)
+                : base(servicesProvider,
+                    dialogService,
+                    dataStore)
         {
             ShuffleCommand = new RelayCommand(ExecuteShuffleCommand, CanExecuteShuffleCommand);
             StopCommand = new RelayCommand(ExecuteStopCommand, CanExecuteStopCommand);
             CheckCommnad = new RelayCommand(ExecuteCheckCommand);
             BackCommand = new RelayCommand(ExecuteBackCommand);
+            NumbersFontSizeUpCommand = new RelayCommand(ExecuteNumbersFontSizeUpCommand);
+            NumbersFontSizeDownCommand = new RelayCommand(ExecuteNumbersFontSizeDownCommand);
+
             HitNumbers = dataStore.HitNumbers;
             _shuffleValues = new ShuffleValues(dataStore.Config.Shuffle.Minimum, dataStore.Config.Shuffle.Maximum, OnShufflingValue);
-        }
-
-        private void ExecuteBackCommand()
-        {
-            VisibleShuffleView = true;
-            VisibleHitNumbersView = false;
-        }
-
-        private void ExecuteCheckCommand()
-        {
-            VisibleShuffleView = false;
-            VisibleHitNumbersView = true;
+            _numbersFontSize = 32.0;
         }
 
         #endregion
@@ -63,6 +76,10 @@
 
         public ICommand BackCommand { get; private set; }
 
+        public ICommand NumbersFontSizeUpCommand { get; private set; }
+
+        public ICommand NumbersFontSizeDownCommand { get; private set; }
+
         public bool Shuffling
         {
             get => _shuffling;
@@ -74,23 +91,23 @@
             get => _currentNumber;
             private set => SetProperty(ref _currentNumber, value);
         }
-        
-        private bool _visibleShuffleView = true;
-        
+
         public bool VisibleShuffleView
         {
             get => _visibleShuffleView;
-            set => SetProperty(ref _visibleShuffleView, value);
+            private set => SetProperty(ref _visibleShuffleView, value);
         }
 
-
-
-        private bool _visibleHitNumbersView;
-        
         public bool VisibleHitNumbersView
         {
             get => _visibleHitNumbersView;
-            set => SetProperty(ref _visibleHitNumbersView, value);
+            private set => SetProperty(ref _visibleHitNumbersView, value);
+        }
+
+        public double NumbersFontSize
+        {
+            get => _numbersFontSize;
+            private set => SetProperty(ref _numbersFontSize, value);
         }
 
         #endregion
@@ -105,6 +122,36 @@
         private bool CanExecuteStopCommand()
         {
             return Shuffling;
+        }
+
+        private void ExecuteBackCommand()
+        {
+            VisibleShuffleView = true;
+            VisibleHitNumbersView = false;
+        }
+
+        private void ExecuteCheckCommand()
+        {
+            VisibleShuffleView = false;
+            VisibleHitNumbersView = true;
+        }
+
+        private void ExecuteNumbersFontSizeDownCommand()
+        {
+            if (_numbersFontSizeIndex > 0)
+            {
+                _numbersFontSizeIndex--;
+                NumbersFontSize = NumbersFontSizeMap[_numbersFontSizeIndex];
+            }
+        }
+
+        private void ExecuteNumbersFontSizeUpCommand()
+        {
+            if (NumbersFontSizeMap.Count - 1 > _numbersFontSizeIndex)
+            {
+                _numbersFontSizeIndex++;
+                NumbersFontSize = NumbersFontSizeMap[_numbersFontSizeIndex];
+            }
         }
 
         private async void ExecuteShuffleCommand()
